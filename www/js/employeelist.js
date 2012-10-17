4578cced524a7e5bcd05bae4a80e2fcf64ce33ac
Kevin Cocco <kcocco@sproutloop.com>
2012-10-17 11:04:46 -0600

    add back deviceready.. build android test next...

--- www/js/employeelist.js
+++ www/js/employeelist.js
@@ -3,7 +3,7 @@ var dbCreated = false;
 
 var scroll = new iScroll('wrapper', { vScrollbar: false, hScrollbar:false, hScroll: false });
 
-// document.addEventListener("deviceready", onDeviceReady, false);
+document.addEventListener("deviceready", onDeviceReady, false);
 onDeviceReady();
 
 function onDeviceReady() {
